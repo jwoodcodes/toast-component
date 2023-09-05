9d94e38a16fdfd701fb4831acecc0013e72fef3f
Jay Wood <jwoodcodes@gmail.com>
2023-09-05 16:30:42 -0500

    exercise 5 done

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -21,7 +21,24 @@ function ToastProvider({children}) {
     
   }
 
-  function DismissToast(id) {
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if(event.code === 'Escape') {
+        setToasts([]);
+      }
+    }
+
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  },[])
+
+
+
+  function DismissToast(id) {     
     const nextToasts = toasts.filter(toast => {
       return toast.id !== id
     })
